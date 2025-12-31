bcb619944bdb1f3f28482b7e736c1c8332109b25
davidzagi <davidzagi93@gmail.com>
2025-12-31 22:09:18 +0100

    Delete concultancys directory

--- concultancys/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactSection from "@/components/sections/contact-us";
-
-export default function Contact() {
-  return (
-    <div>
-      <ContactSection  textcontent="text-white" textheading="text-white" backgroundColor="bg-[#0A1236]/95"  />
-    </div>
-  );
-}
